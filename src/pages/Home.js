c375b27e0576c00c865980384efc286bef084ca0
takamamacodes <wcomurray@gmail.com>
2021-08-10 00:24:50 -0400

    Edited content for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ class HomePage extends React.Component {
           <Col xs={6} md={4}>
             <h3 class="text-center">Thanks for taking a look</h3>
             <p class="text-justify">To the left you'll see me holding the namesake of my GitHub handle. Taka is a 1.5 year old goldendoodle and my fur-baby. He is 60 pounds of love (and also mischief) and he never fails to brighten every day!</p>
-            <p class="text-justify">Bringing Taka into my life in the midst of COVID-19 was one of the biggest changes I have ever experienced in my life. Just learning to be 100% responsible for the wellbeing of another creature is an enormous thing. One that I am always so impressed others handle with such grace. But besides being a typical puppy with typical puppy antics, Taka is one of many "pandemic puppies" with separation anxiety. Because I spend a <b>lot</b> of time at home, I thought it would be fun to brush up on my technical skills and try my hand at building a personal website. Tadah!</p>
+            <p class="text-justify">Bringing Taka into my life in the midst of COVID-19 was one of the biggest changes I have ever experienced. Just learning to be 100% responsible for the wellbeing of another creature is an enormous thing. One that I am always so impressed others handle with such grace. But besides being a typical puppy with typical puppy antics, Taka is one of many "pandemic puppies" with separation anxiety. Because I spend a <b>lot</b> of time at home, I thought it would be fun to brush up on my technical skills and try my hand at building a personal website. Tadah!</p>
           </Col>
         </Row>
       </Container>
